270b2086bab4e6e4cb957e7e6a1e01f8fc90b04a
meheraaj <battleshipnb@gmail.com>
2026-06-20 17:28:53 +0600

    Initial Project setup

---
+++ src/app.js
@@ -0,0 +1,15 @@
+//Import
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({
+    status: "Ok",
+  });
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Running on Port " + process.env.PORT || 3000);
+});
