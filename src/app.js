008e48939703bd2436a598e02905f5632fd1728a
meheraaj <battleshipnb@gmail.com>
2026-06-24 11:38:13 +0600

    Added db connection file

--- src/app.js
+++ src/app.js
@@ -1,15 +1,24 @@
 //Import
 const express = require("express");
+
+const pool = require("./config/db.js");
 require("dotenv").config();
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.json({
-    status: "Ok",
-  });
+//database test
+app.get("/testdb", async (req, res) => {
+  try {
+    const ress = await pool.query("SELECT NOW()");
+    console.log("Connected:", ress.rows[0]);
+    res.json({
+      ress,
+    });
+  } catch (err) {
+    console.error(err);
+  }
 });
 
 app.listen(process.env.PORT || 3000, () => {
