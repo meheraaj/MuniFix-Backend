91c524810a6707a5e6fbd75744ff6862f36e2151
meheraaj <battleshipnb@gmail.com>
2026-06-29 11:28:48 +0600

    complain

--- src/controllers/citizen.controller.js
+++ src/controllers/citizen.controller.js
@@ -1,1 +1,16 @@
-export const addNewComplain = async (req, res, next) => {};
+import { response } from "express";
+
+export const addNewComplain = async (req, res, next) => {
+  const {
+    longitude,
+    latitude,
+    city,
+    street,
+    country,
+    image,
+    title,
+    description,
+  } = req.body;
+
+  response.status(200).json(req.body);
+};
