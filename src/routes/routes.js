838af745b9c20ab6e6f3553902a10602bc1dd3df
meheraaj <battleshipnb@gmail.com>
2026-07-30 11:46:40 +0600

    Final

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -7,13 +7,19 @@ const admin_routes = require("./admin.routes.js");
 const notification_routes = require("./notification.routes.js");
 
 const routes = express.Router();
-
+routes.get("/", (req, res) => {
+    res.status(200).json({
+        success: true,
+        message: "Welcome to MuniFix API",
+    });
+})
 routes.use("/auth", auth_routes);
 routes.use("/complain", complain_routes);
 routes.use("/my", profile_routes);
 routes.use("/citizen", citizen_routes);
 routes.use("/admin", admin_routes);
 routes.use("/notifications", notification_routes);
+
 
 module.exports = routes;
 
--- src/utils/apiError.js
+++ src/utils/apiError.js
@@ -3,7 +3,7 @@ class ApiError extends Error {
     super(message);
     this.statusCode = statusCode;
     this.success = false;
-    Error.captureStackTrace(this, this.constructor);
+    
   }
 }
 
