e9c437ebfd9402edff5ed5c7fac337e31d3f78f0
meheraaj <battleshipnb@gmail.com>
2026-08-05 09:23:58 +0600

    cv

--- src/app.js
+++ src/app.js
@@ -53,7 +53,7 @@ app.use((err, req, res, next) => {
 });
 
 module.exports = app;
-if (process.env.NODE_ENV !== "production") {
+if (process.env.NODE_ENV !== "production" && process.env.NODE_ENV !== "test") {
   const PORT = process.env.PORT || 3000;
   app.listen(PORT, () => {
     console.log(`Running on Port ${PORT}`);
